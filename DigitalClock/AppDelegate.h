3e396943ab4368f51a1f1a039a8a350fc10ab995
elizeurs <13ders@gmail.com>
2020-01-18 20:47:08 -0300

    Initial Commit

---
+++ DigitalClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DigitalClock
+//
+//  Created by Elizeu RS on 18/01/20.
+//  Copyright © 2020 elizeurs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DigitalClock/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DigitalClock
+//
+//  Created by Elizeu RS on 18/01/20.
+//  Copyright © 2020 elizeurs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
